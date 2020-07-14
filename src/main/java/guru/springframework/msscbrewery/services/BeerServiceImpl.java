1e1d629014880d022b9ac6bebfc9eb314adde8e9
nursace <34605644+nursace@users.noreply.github.com>
2020-07-14 11:45:22 +0600

    First Commit

---
+++ src/main/java/guru/springframework/msscbrewery/services/BeerServiceImpl.java
@@ -0,0 +1,33 @@
+package guru.springframework.msscbrewery.services;
+
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+    @Override
+    public BeerDto getBeerById(UUID beerId) {
+        return BeerDto.builder().id(UUID.randomUUID())
+                .name("Galaxy Cat")
+                .style("Pale Ale")
+                .build();
+    }
+
+    @Override
+    public BeerDto saveNewBeer(BeerDto beerDto) {
+        return BeerDto.builder().id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateBeer(UUID beerId) {
+
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+
+    }
+}
